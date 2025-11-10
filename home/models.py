3e194e64e94742722c2316cfbcdc443405053485
App Generator <51070104+app-generator@users.noreply.github.com>
2025-11-10 14:54:10 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    email = models.CharField(max_length=255, null=True, blank=True)
+    organisation = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Contract(models.Model):
+
+    #__Contract_FIELDS__
+    title = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Contract_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Contract")
+        verbose_name_plural = _("Contract")
+
+
+
+#__MODELS__END
